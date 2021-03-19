c18a68b723113a0765e55f45dd61ff5db7fe9362
yokobond <yokobond@users.noreply.github.com>
2021-03-19 10:32:38 +0000

    deploy: b6bdee09837da41d343f62438c1ee47507ec247b

---
+++ src/entry/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {FormattedMessage} from 'react-intl';
+
+import keyEventsIconURL from './entry-icon.png';
+import keyEventsInsetIconURL from './inset-icon.svg';
+
+const translationMap = {
+    'ja': {
+        'gui.extension.keyEvents.description': 'Xcratch 拡張の例'
+    },
+    'ja-Hira': {
+        'gui.extension.keyEvents.description': 'Xcratch (えくすくらっち)かくちょうのれい'
+    }
+};
+
+const entry = {
+    name: 'Key Events',
+    extensionId: 'keyEvents',
+    extensionURL: 'https://yokobond.github.io/xcx-key-events/dist/keyEvents.mjs',
+    collaborator: 'Yengawa Lab',
+    iconURL: keyEventsIconURL,
+    insetIconURL: keyEventsInsetIconURL,
+    description: (
+        <FormattedMessage
+            defaultMessage="example extension for Xcratch"
+            description="Description for example extension for Xcratch"
+            id="gui.extension.keyEvents.description"
+        />
+    ),
+    featured: true,
+    disabled: false,
+    bluetoothRequired: false,
+    internetConnectionRequired: true,
+    helpLink: 'https://github.com/yokobond/xcx-key-events/',
+    translationMap: translationMap
+};
+
+export {entry}; // loadable-extension needs this line.
+export default entry;
